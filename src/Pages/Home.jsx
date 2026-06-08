bf0c176636214cd3c7b195f49a434e707281a0d8
XJuanCarlosXD <juancarlos192003@gmail.com>
2026-06-08 15:02:03 -0400

    master

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import { MotionProvider } from "../Components/motion/space-primitives";
 import Hero from "../Components/rebrand/Hero";
 import Products from "../Components/rebrand/Products";
 import Services from "../Components/rebrand/Services";
-import TeamSection from "../Components/rebrand/TeamSection";
+// import TeamSection from "../Components/rebrand/TeamSection";
 import CTA from "../Components/rebrand/CTA";
 import Footer from "../Components/rebrand/Footer";
 
